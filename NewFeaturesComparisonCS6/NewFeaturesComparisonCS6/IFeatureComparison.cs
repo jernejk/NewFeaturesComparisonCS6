305e9f5373e9e1070e47af70db44dad0d580a6a7
jernejk <jernej.kavka@windowslive.com>
2015-06-14 14:56:35 +0200

    Interface and a few tests for comparisions between C# 5 and 6.

---
+++ NewFeaturesComparisonCS6/NewFeaturesComparisonCS6/IFeatureComparison.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NewFeaturesComparisonCS6
+{
+    public interface IFeatureComparison
+    {
+        /// <summary>
+        /// Simple event handler.
+        /// </summary>
+        event EventHandler OperationCompleted;
+
+        /// <summary>
+        /// Gets a read only property value.
+        /// </summary>
+        bool IsReadOnly { get; }
+
+        /// <summary>
+        /// Gets the name of a property (in this case IsReadOnly property)
+        /// </summary>
+        string IsReadOnlyPropertyName { get; }
+
+        /// <summary>
+        /// Simple Math.Pow calculation.
+        /// </summary>
+        double Compute(double a, double b);
+
+        /// <summary>
+        /// A bit more complex async method when using C# 5 language.
+        /// Try, catch and finally contains async methods and awaits are required.
+        /// </summary>
+        Task DoSomeWorkWithTryCatchFinallyAsync();
+
+        /// <summary>
+        /// Simple event invocation.
+        /// </summary>
+        void RaiseEvent();
+
+        /// <summary>
+        /// This method will throw a exception while also logging that exception before exception bubbles up
+        /// </summary>
+        void ThrowAndLogException();
+
+        /// <summary>
+        /// A simple demonstration of a dictionary settings.
+        /// </summary>
+        /// <returns>Returns dictionary of settings.</returns>
+        Dictionary<string, string> GetSettings();
+
+        /// <summary>
+        /// Formats a settings based on their key.
+        /// It should gracefully handle missing keys and null settings values.
+        /// </summary>
+        /// <param name="key">Setting key</param>
+        /// <returns>Returns information about a setting.</returns>
+        string GetFormattedSettingValue(string key);
+    }
+}
--- NewFeaturesComparisonCS6/NewFeaturesComparisonCS6/Program.cs
+++ NewFeaturesComparisonCS6/NewFeaturesComparisonCS6/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using static System.Console;
 
 namespace NewFeaturesComparisonCS6
 {
@@ -10,6 +6,30 @@ namespace NewFeaturesComparisonCS6
     {
         static void Main(string[] args)
         {
+        }
+
+        private static async void ShowStuff(IFeatureComparison stuff)
+        {
+            stuff.OperationCompleted += Stuff_OperationCompleted;
+
+            WriteLine("Is readonly property name: " + stuff.IsReadOnlyPropertyName);
+            WriteLine("Is readonly: " + stuff.IsReadOnly);
+            WriteLine("Compute: " + stuff.Compute(2, 4));
+
+            WriteLine("Setting value for EnableStuff: " + stuff.GetFormattedSettingValue("EnableStuff"));
+            WriteLine("Setting value for NonExisting: " + stuff.GetFormattedSettingValue("NonExisting"));
+            WriteLine("Setting value for ExsitingWithNullValue: " + stuff.GetFormattedSettingValue("ExsitingWithNullValue"));
+
+            await stuff.DoSomeWorkWithTryCatchFinallyAsync();
+
+            WriteLine("---------------------");
+
+            stuff.OperationCompleted -= Stuff_OperationCompleted;
+        }
+
+        private static void Stuff_OperationCompleted(object sender, System.EventArgs e)
+        {
+            WriteLine("DoSomeWorkWithTryCatchFinallyAsync completed.");
         }
     }
 }

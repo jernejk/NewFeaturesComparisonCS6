8d905ca384c00abfd01dc0c5f1944080822cdbea
jernejk <jernej.kavka@windowslive.com>
2015-06-14 14:31:38 +0200

    First commit.

---
+++ NewFeaturesComparisonCS6/NewFeaturesComparisonCS6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewFeaturesComparisonCS6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
